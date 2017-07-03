aa84c2cc30da979ce1c7e5679bef15bb09b4a393
Leonardo Val <leonardo.val@creatartis.com>
2017-07-03 13:07:01 -0300

    Memory stats
    + Added memory usage statistics.

--- src/capataz_node/server.js
+++ src/capataz_node/server.js
@@ -1,14 +1,14 @@
 /** # Capataz server ([Node](http://nodejs.org))
 
-The Capataz constructor builds Capataz servers which can schedule jobs and assign them to connecting 
+The Capataz constructor builds Capataz servers which can schedule jobs and assign them to connecting
 browsers. It is based on a [ExpressJS](http://expressjs.com/) application.
 */
 var Capataz = exports.Capataz = declare({
 	constructor: function Capataz(config) {
-		/** The `config` property holds configuration options. Some will be shown to the clients, 
+		/** The `config` property holds configuration options. Some will be shown to the clients,
 		which may override them with URL's query arguments.
-		
-		The parameters that deal with the workload distribution are: 
+
+		The parameters that deal with the workload distribution are:
 		*/
 		initialize(this.config = {}, config)
 		/** + `workerCount = 2` controls how many web workers the clients spawn. This may be changed
@@ -21,11 +21,11 @@ var Capataz = exports.Capataz = declare({
 		*/
 			.integer('useWebworkers', { defaultValue: 1, coerce: true })
 		/** + `adjustWorkerCount = true` makes the client to set the `workerCount` to the value of
-		[`navigator.hardwareConcurrency`](https://wiki.whatwg.org/wiki/Navigator_HW_Concurrency) if 
+		[`navigator.hardwareConcurrency`](https://wiki.whatwg.org/wiki/Navigator_HW_Concurrency) if
 		available.
 		*/
 			.bool('adjustWorkerCount', { defaultValue: true, coerce: true })
-		/** + `maxRetries = 100` defines how many times the clients should retry failed 
+		/** + `maxRetries = 100` defines how many times the clients should retry failed
 		connections to the server.
 		*/
 			.number('maxRetries', { defaultValue: 100, coerce: true })
@@ -35,42 +35,42 @@ var Capataz = exports.Capataz = declare({
 		/** + `maxDelay = 15m` sets the maximum delay between retries.
 		*/
 			.number('maxDelay', { defaultValue: 15 * 60000 , coerce: true })
-		
-		/** + `desiredEvaluationTime = 10s` defines the expected time the browsers must spend 
+
+		/** + `desiredEvaluationTime = 10s` defines the expected time the browsers must spend
 		on each task. The server will bundle jobs if clients execute them faster than this value.
 		*/
 			.number('desiredEvaluationTime', { defaultValue: 10000, coerce: true })
 		/** + `maxTaskSize = 50` is the maximum amount of jobs that can be bundled per task.
 		*/
 			.number('maxTaskSize', { defaultValue: 50, coerce: true })
-		/** + `evaluationTimeGainFactor = 90%` is the gain factor used to adjust the 
-		evaluation time average.		
+		/** + `evaluationTimeGainFactor = 90%` is the gain factor used to adjust the
+		evaluation time average.
 		*/
 			.number('evaluationTimeGainFactor', { defaultValue: 0.9, coerce: true })
-		/** + `maxScheduled = 5000` is the maximum amount of scheduled pending jobs at any 
+		/** + `maxScheduled = 5000` is the maximum amount of scheduled pending jobs at any
 		given time. Trying to schedule more jobs will raise an exception.
 		*/
 			.number('maxScheduled', { defaultValue: 5000, coerce: true })
 		/** The parameters that deal with the [ExpressJS](http://expressjs.com/) server are:
-		
+
 			+ `port = 8080` is the port the server will listen to.
 		*/
 			.integer('port', { defaultValue: 8080, coerce: true })
 		/** + `staticRoute = /capataz` is the URL from where static files are served.
 		*/
 			.string('staticRoute', { defaultValue: '/capataz' })
-		/** + `serverFiles = <module_path>/static` is the path from where the static files of 
+		/** + `serverFiles = <module_path>/static` is the path from where the static files of
 		Capataz will be served.
-		*/	
+		*/
 			.string('serverFiles', { defaultValue: path.dirname(module.filename) +'/static' })
-		/** + `customFiles = ''` is the path (or paths, separated by `'\n'`) from where custom 
+		/** + `customFiles = ''` is the path (or paths, separated by `'\n'`) from where custom
 		static files will be served.
-		*/	
+		*/
 			.string('customFiles', { defaultValue: '' })
 		/** + `routes = {}` is an object that may redefine the URLs of the Capataz verbs.
 		*/
 			.object('routes', { defaultValue: {} })
-		/** + `authentication`: a function with signature `function (url, username, password)` to 
+		/** + `authentication`: a function with signature `function (url, username, password)` to
 			use for basic authentication.
 		*/
 			.func('authentication', { ignore: true })
@@ -102,19 +102,20 @@ var Capataz = exports.Capataz = declare({
 		}
 		this.__startTime__ = Date.now();
 	},
-	
+
 	/** To schedule a new job the following data must be provided (in `params`):
-	
+
 		+ `fun`: Either a function or a string with the Javascript code of a function.
 		+ `imports`: Array of dependencies to be loaded with [requirejs](http://requirejs.org/).
 			These will be the first arguments in the call to `fun`.
 		+ `args`: Further arguments to pass to `fun`, after the dependencies.
-		+ `info`: Description of the job to be displayed to the user. 
-		
+		+ `info`: Description of the job to be displayed to the user.
+
 	The result of a call to `schedule` is a future that will be resolved when the job is executed by
 	a worker client.
 	*/
 	schedule: function schedule(params) {
+		this.__accountServerStats__();
 		if (!params.fun) {
 			raise("Cannot schedule ", JSON.stringify(params), ", it has no `fun`!");
 		}
@@ -128,20 +129,28 @@ var Capataz = exports.Capataz = declare({
 		job.fail(this.logger.error.bind(this.logger));
 		return job;
 	},
-	
-	/** The `taskSize` is calculated in order to force the clients to spend approximately the 
-	`desiredEvaluationTime`. In order to minimize network and compilation time clients must not 
+
+	__accountServerStats__: function __accountServerStats__() {
+		var stats = this.statistics,
+			memoryUsage = process.memoryUsage();
+		['rss', 'heapUsed', 'heapTotal'].forEach(function (t) {
+			stats.add({key: 'memory', type: t}, memoryUsage[t]);
+		});
+	},
+
+	/** The `taskSize` is calculated in order to force the clients to spend approximately the
+	`desiredEvaluationTime`. In order to minimize network and compilation time clients must not
 	spend too little nor too much time executing jobs.
 	*/
 	taskSize: function taskSize() {
 		var statEJT = this.statistics.stat({ key: 'estimated_time' }),
 			estimatedJobTime = Math.max(1, statEJT.average());
-		return statEJT.count() <= 3 ? 1 : Math.round(Math.max(1, 
+		return statEJT.count() <= 3 ? 1 : Math.round(Math.max(1,
 			Math.min(this.config.maxTaskSize, this.config.desiredEvaluationTime / estimatedJobTime)
 		));
 	},
-	
-	/** The function `nextTask` builds a new task, which is a bundle of jobs sent to the client 
+
+	/** The function `nextTask` builds a new task, which is a bundle of jobs sent to the client
 	to be executed. If an `amount` is not given, one is calculated with `taskSize()`.
 	*/
 	nextTask: function nextTask(amount) {
@@ -162,8 +171,8 @@ var Capataz = exports.Capataz = declare({
 			})
 		};
 	},
-	
-	/** A post isn't valid when: it has no `assignedSince` timestamp, it was assigned before the 
+
+	/** A post isn't valid when: it has no `assignedSince` timestamp, it was assigned before the
 	server came online (it is outdated) or in the future, or it has a weird evaluation time (zero or
 	less).
 	*/
@@ -174,12 +183,13 @@ var Capataz = exports.Capataz = declare({
 			post.time <= 0 // Result evaluation time is zero or negative!
 		;
 	},
-	
-	/** When all jobs in a task have been completed, the clients post the results back to the 
-	server. The function `processResult(post)` checks the posted result, and if it is valid, fulfils 
+
+	/** When all jobs in a task have been completed, the clients post the results back to the
+	server. The function `processResult(post)` checks the posted result, and if it is valid, fulfils
 	the corresponding jobs.
 	*/
 	processResult: function processResult(post) {
+		this.__accountServerStats__();
 		var job = this.store.assigned(post.id),
 			status;
 		if (!job) { // Result's job id does not exist.
@@ -199,26 +209,26 @@ var Capataz = exports.Capataz = declare({
 			this.accountPost(post, job, status);
 		}
 	},
-	
+
 	/** Gathers statistics about completed jobs. Some are used in the server's own operation.
 	*/
 	accountPost: function accountPost(post, job, status) {
 		if (status === 'resolved') {
-			this.statistics.gain({key: 'estimated_time'}, post.time, 
+			this.statistics.gain({key: 'estimated_time'}, post.time,
 				this.config.evaluationTimeGainFactor);
 		}
 		this.statistics.add(copy({key: 'evaluation_time', status: status,
 			platform: post.clientPlatform, client: post.postedFrom,
 		}, job && job.tags), post.time);
-		this.statistics.add(copy({key: 'roundtrip_time', status: status, 
+		this.statistics.add(copy({key: 'roundtrip_time', status: status,
 			platform: post.clientPlatform, client: post.postedFrom
 		}, job && job.tags), Date.now() - post.assignedSince);
 	},
-	
+
 	// ## Request handlers. ########################################################################
-	
-	/** The clients fetch their configuration via a `GET` to `/config.json`. This is handled by 
-	`get_config(request, response)`. The client's configuration is a JSON object with parameters 
+
+	/** The clients fetch their configuration via a `GET` to `/config.json`. This is handled by
+	`get_config(request, response)`. The client's configuration is a JSON object with parameters
 	like the amount of retries and the delays between them.
 	*/
 	get_config: function serveConfig(request, response) {
@@ -233,9 +243,9 @@ var Capataz = exports.Capataz = declare({
 			useWebworkers: this.config.useWebworkers
 		});
 	},
-	
-	/** The clients fetch a new task via a `GET` to `/task.json`. This is handled by 
-	`get_task(request, response)`. A task is a JSON object with one or more jobs; each including the 
+
+	/** The clients fetch a new task via a `GET` to `/task.json`. This is handled by
+	`get_task(request, response)`. A task is a JSON object with one or more jobs; each including the
 	code to be executed, the job's id, and other related data.
 	*/
 	get_task: function get_task(request, response) {
@@ -251,8 +261,8 @@ var Capataz = exports.Capataz = declare({
 		}
 		return true;
 	},
-	
-	/** The clients report a task's results via a `POST` to `/task.json`. This is handled by 
+
+	/** The clients report a task's results via a `POST` to `/task.json`. This is handled by
 	`post_task(request, response)`.
 	*/
 	post_task: function post_task(request, response) {
@@ -268,7 +278,7 @@ var Capataz = exports.Capataz = declare({
 			});
 		}
 	},
-	
+
 	/** The clients can get the server's statistics via a `GET` to `/stats.json`. This is handled by
 	`get_stats(request, response)`.
 	*/
@@ -276,20 +286,20 @@ var Capataz = exports.Capataz = declare({
 		response.set("Cache-Control", "max-age=0,no-cache,no-store");
 		response.json(this.statistics);
 	},
-	
-	/** The clients can get the server's store status via a `GET` to `/store.json`. This is handled 
+
+	/** The clients can get the server's store status via a `GET` to `/store.json`. This is handled
 	by `get_store(request, response)`.
 	*/
 	get_store: function get_store(request, response) {
 		response.set("Cache-Control", "max-age=0,no-cache,no-store");
 		response.json(this.store.status());
 	},
-	
+
 	// ## ExpressJS. ###############################################################################
 
-	/** The Capataz server relays on a [ExpressJS](http://expressjs.com/) application. The method 
-	`configureApp(app)` configures the given ExpressJS app, or a new one if none is provided. 
-	Configuration includes setting up the routes (including static files), JSON parsing, compression 
+	/** The Capataz server relays on a [ExpressJS](http://expressjs.com/) application. The method
+	`configureApp(app)` configures the given ExpressJS app, or a new one if none is provided.
+	Configuration includes setting up the routes (including static files), JSON parsing, compression
 	and (of course) the method handlers.
 	*/
 	configureApp: function configureApp(app) {
@@ -321,7 +331,7 @@ var Capataz = exports.Capataz = declare({
 		});
 		return app;
 	},
-	
+
 	/** `Capataz.run` is a shortcut to quickly configure and start a Capataz server. The `config`
 	argument may include an ExpressJS `app` to use.
 	*/
@@ -334,11 +344,11 @@ var Capataz = exports.Capataz = declare({
 		capataz.logger.info('Server started and listening at port ', port, '.');
 		return capataz;
 	},
-	
+
 	// ## Utilities. ###############################################################################
 
-	/** Many times the amount of jobs is too big for the server to schedule all at once. The 
-	function `scheduleAll()` takes a job generator (`jobs`). It will take an `amount` of jobs from 
+	/** Many times the amount of jobs is too big for the server to schedule all at once. The
+	function `scheduleAll()` takes a job generator (`jobs`). It will take an `amount` of jobs from
 	the generator and schedule them, and wait for them to finish before proceeding to the next jobs.
 	In this way all jobs can be handled without throttling the server. The `callback` is called for
 	every job actually scheduled with the resulting `Future`.
@@ -358,7 +368,7 @@ var Capataz = exports.Capataz = declare({
 			} catch (err) {
 				Iterable.prototype.catchStop(err);
 			}
-			
+
 			return partition.length < 1 ? false : Future.all(partition);
 		} /* Future.all() result is an array, hence always truthy. */ );
 	}
