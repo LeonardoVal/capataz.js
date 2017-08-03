07adb09992e4d85ffddb4d0063a0d8b0e0d7277d
Leonardo Val <leonardo.val@creatartis.com>
2017-08-03 00:37:10 -0300

    Sermat
    + Considering using Sermat instead of JSON.

--- src/capataz_node/__prologue__.js
+++ src/capataz_node/__prologue__.js
@@ -4,19 +4,21 @@
 	/** This module depends on [ExpressJS](http://expressjs.com/),
 	[`creatartis-base`](https://github.com/LeonardoVal/creatartis-base) and others.
 	*/
-	var base = require('creatartis-base'),
+	var path = require('path'),
+		filesystem = require('fs'),
 		express = require('express'),
-		path = require('path'),
-		filesystem = require('fs');
-	
+		base = require('creatartis-base'),
+		Sermat = require('sermat');
+	Sermat.modifiers.mode = Sermat.CIRCULAR_MODE;
+	Sermat.include('Function');
+
 	/** Dependencies are exported so they may be used by user code.
 	*/
+	exports.__package__ = 'capataz_node';
 	exports.__name__ = 'capataz_node';
-	exports.__dependencies__ = { 
-		base: base, 
-		express: express 
-	};
-	
+	exports.__dependencies__ = [express, base, Sermat];
+	exports.__SERMAT__ = { include: [] };
+
 	/** Import synonyms.
 	*/
 	var copy = base.copy,
