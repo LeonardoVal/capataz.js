ffbed64042433b1fc4a7291aa90076c703e5bab7
Leonardo Val <leonardo.val@creatartis.com>
2017-08-03 00:35:29 -0300

    Job code wrapper
    + Better encapsulate job's code wrapper.

--- src/capataz_worker.js
+++ src/capataz_worker.js
@@ -1,11 +1,11 @@
 ﻿/** # Capataz [web worker](http://www.w3schools.com/html/html5_webworkers.asp)
 
 This module defines the web worker part of a Capataz client. This is the actual
-worker that executes the jobs' code in the client machine. 
+worker that executes the jobs' code in the client machine.
 */
 (function (self) { "use strict";
 	/** A provisional event handler prevents jobs to be passed before the web worker
-	has properly initialized. 
+	has properly initialized.
 	*/
 	self.onmessage = function (msg) {
 		var data = JSON.parse(msg.data);
@@ -22,21 +22,26 @@ worker that executes the jobs' code in the client machine.
 	*/
 	require(['creatartis-base'], function (base) {
 		self.base = base;
+
+		var CODE = '(function () {"use strict";\n'+ // Job wrapper.
+			'\treturn base.Future.imports.apply(this, $imports).then(function (deps) {\n'+
+				'\t\treturn ($fun).apply(this, deps.concat($args));\n'+
+			'\t});\n'+
+		'})()';
+
 		/** The `onmessage` event handler is updated to receive jobs, execute them
 		and send the result back to the rendering thread.
 		*/
 		self.onmessage = function onmessage(msg) {
 			var data = JSON.parse(msg.data),
-				code = '(function () {"use strict";\n'+ // Job wrapper.
-					'\treturn base.Future.imports.apply(this, '+ JSON.stringify(data.imports || []) +').then(function (deps) {\n'+
-						'\t\treturn ('+ data.fun +').apply(this, deps.concat('+ JSON.stringify(data.args || []) +'));\n'+
-					'\t});\n'+
-				'})()';
-			
+				code = CODE
+					.replace('$imports', JSON.stringify(data.imports || []))
+					.replace('$fun', data.fun)
+					.replace('$args', JSON.stringify(data.args || []));
 			base.Future.invoke(eval, self, code).then(function (result) {
 				data.result = result;
 				self.postMessage(JSON.stringify(data));
-			}, function (error) { 
+			}, function (error) {
 				data.error = 'Execution failed with "'+ error +'"!\nCode:\n'+ code +'\nCallstack:\n\t'+
 					base.callStack(error).join('\n\t'); // Error instances are not serializable.
 				self.postMessage(JSON.stringify(data));
